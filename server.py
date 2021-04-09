bb6c7e954e486af38a8a05b38b68b18fb71ea805
JulienConan <72304768+JulienConan@users.noreply.github.com>
2021-04-09 17:22:18 +0200

    Update server.py

--- server.py
+++ server.py
@@ -53,9 +53,12 @@ def purchasePlaces():
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
-    competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
-    club['points'] = int(club['points'])-placesRequired 
-    flash('Great-booking complete!')
+    if placesRequired > 12:
+        flash("You shouldn't be able to book more than 12 places per competition.")
+    else:
+        competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
+        club['points'] = int(club['points'])-placesRequired 
+        flash('Great-booking complete!')
     return render_template('welcome.html', club=club, competitions=competitions)
 
 
